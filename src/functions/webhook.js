663bac0f0bc72a9477592e44fd530560da577900
Rob van der Leek <robvanderleek@gmail.com>
2022-10-13 05:21:19 +0200

    Say hello world

--- src/functions/webhook.js
+++ src/functions/webhook.js
@@ -1,7 +1,8 @@
 const handler = async (event, context) => {
     console.log('Handler called!');
     return {
-        statusCode: 200
+        statusCode: 200,
+        body: '<html><body><h1>Hello world!</h1></body></html>'
     };
 }
 
