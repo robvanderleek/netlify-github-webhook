5cf44e8b7f04a60ec1b0eecbbbf4bbfcaf35043f
Rob van der Leek <robvanderleek@gmail.com>
2022-10-13 23:03:29 +0200

    Comment on PR

--- src/functions/webhook.js
+++ src/functions/webhook.js
@@ -1,5 +1,24 @@
+const {Octokit} = require("@octokit/rest");
+
 const handler = async (event, context) => {
-    console.log('Handler called!');
+    const method = event.httpMethod;
+    if (method !== 'POST') {
+        return {statusCode: 403}
+    }
+    const payload = JSON.parse(event.body);
+    if (payload.action !== 'opened' && payload.action !== 'reopened') {
+        return {statusCode: 201}
+    }
+    const octokit = new Octokit({auth: process.env.NETLIFY_WEBHOOK_GITHUB_TOKEN});
+    const base = payload.pull_request.base;
+    const owner = base.repo.owner.login;
+    const repo = base.repo.name;
+    await octokit.rest.issues.createComment({
+        owner: owner,
+        repo: repo,
+        issue_number: payload.number,
+        body: 'Hello world'
+    });
     return {
         statusCode: 200,
         body: '<html><body><h1>Hello world!</h1></body></html>'
